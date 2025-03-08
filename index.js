df11a1cb25b6890f6287d6f77ca4128415a4eba8
Raghu Poudel <118181558+raghupoudel@users.noreply.github.com>
2025-03-08 14:17:15 +0545

    commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// window.onload = function() {
+//     // Show the greeting page on load
+//     var greetingPage = document.querySelector('.greeting-page');
+//     greetingPage.style.display = 'flex';
+
+//     // Hide the greeting page after 2 seconds
+//     setTimeout(function() {
+//         greetingPage.style.display = 'none';
+//     }, 2000);
+// }
+
+// // Custom cursor functionality
+// let cursor = document.querySelector('.cursor');
+
+// // Move the cursor to the mouse position
+// document.addEventListener('mousemove', (e) => {
+//     cursor.style.left = e.pageX + 'px';
+//     cursor.style.top = e.pageY + 'px';
+// });
